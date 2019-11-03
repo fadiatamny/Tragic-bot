633d6916830d5ef7203e4c25c813ce5d049bd248
Fadi Atamny <fadiatamny@gmail.com>
2019-11-03 17:49:23 +0200

    added async based music bot

--- bot.py
+++ bot.py
@@ -9,6 +9,29 @@ class Tragic(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.queue = asyncio.Queue()
+        self.pNext = asyncio.Event()
+
+    @staticmethod
+    async def task(self):
+        while True:
+            self.pNext.clear()
+            curr = await self.queue.get()
+            curr.start()
+            await pNext.wait()
+
+    def toggle(self):
+        self.bot.loop.call_soon_threadsafe(pNext.set)
+
+    @commands.command()
+    async def play(self, ctx, url):
+        if not bot.is_voice_connected(ctx.message.server):
+            v = await bot.join_voice_channel(ctx.message.author.voice_channel)
+        else:
+            v = bot.voice_client_in(ctx.message.server)
+        
+        p = await v.create_ytdl_player(url, after=toggle)
+        await self.queue.put(p)
 
     @commands.command()
     async def hi(self, ctx, args):
@@ -17,10 +40,11 @@ class Tragic(commands.Cog):
 
 bot = commands.Bot(command_prefix = commands.when_mentioned_or(prefix), description= "random test woo")
         
+bot.add_cog(Tragic(bot))
+
 @bot.event
 async def on_ready():
     print("Connected! as\t" + str(bot.user.name) + str(bot.user.id))
-
-bot.add_cog(Tragic(bot))
+    bot.loop.create_task(Tragic.task())
 
 bot.run(token)
